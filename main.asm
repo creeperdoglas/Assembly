538354882a65cd0d3481ab86a826982578e8601f
Melker Gustafsson <77453395+creeperdoglas@users.noreply.github.com>
2023-11-12 16:29:50 +0100

    Add files via upload

---
+++ main.asm
@@ -0,0 +1,43 @@
+;
+; IR LABB.asm
+;
+; Created: 2023-11-12 16:24:50
+; Author : Melker Gustafsson
+;
+
+
+; Initiera systemet
+Start:
+    ldi r16, 0xFF
+    out DDRB, r16          ; Sätt PORTB som utgång
+    ldi r16, HIGH(RAMEND)
+    out SPH, r16           ; Sätt höga pekaren
+    ldi r16, LOW(RAMEND)
+    out SPL, r16           ; Sätt låga pekaren
+
+; Vänta på startbit
+WaitForStart:
+    sbis PINA, 0
+    rjmp WaitForStart
+
+; Läs och bearbeta data
+ReadData:
+    ldi r16, 4             ; Räknare för databitar
+ProcessLoop:
+    call Delay
+    lsl r20                ; Skifta r20 åt vänster
+    sbic PINA, 0
+    inc r20                ; Öka r20 om PINA.0 är hög
+    dec r16
+    brne ProcessLoop
+
+; Skicka ut data
+    out PORTB, r20         ; Skicka bearbetade data till PORTB
+
+; Återställ och upprepa
+    rjmp Start              ; Upprepa processen
+
+; Fördröjningsrutin
+Delay:
+    
+    ret
